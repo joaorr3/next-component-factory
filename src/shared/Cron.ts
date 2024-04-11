7f88558bcc65426ef9d77b01493cf25fa48bc5e3
João Ribeiro <X210091@bcpcorp.net>
2024-04-11 10:14:26 +0100

    add Cron logs

--- src/shared/Cron.ts
+++ src/shared/Cron.ts
@@ -1,7 +1,9 @@
-import cron from "node-cron";
+import cron, { type ScheduleOptions } from "node-cron";
+import logger from "./logger";
 
-const cronOptions = {
+const cronOptions: Omit<ScheduleOptions, "name"> = {
   timezone: "Europe/Lisbon",
+  scheduled: true,
 };
 
 export const Cron = (interval: string, cb: () => void, id: string) => {
@@ -12,7 +14,15 @@ export const Cron = (interval: string, cb: () => void, id: string) => {
     task.stop();
   }
 
-  const job = cron.schedule(interval, cb, { ...cronOptions, name: id });
+  const event = () => {
+    logger.console.server({
+      level: "info",
+      message: `Cron ${id}:`,
+    });
+    cb();
+  };
+
+  const job = cron.schedule(interval, event, { ...cronOptions, name: id });
   job.start();
   return job;
 };
